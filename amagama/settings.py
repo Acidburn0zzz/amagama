6685cc209bb666d4d9b83cb9cf53493a4f555e57
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-02-27 10:59:42 +0100

    UNDO

--- amagama/settings.py
+++ amagama/settings.py
@@ -1,17 +1,17 @@
 # Global config
 
 DEBUG = False
-ENABLE_WEB_UI = False
-ENABLE_DATA_ALTERING_API = False
+ENABLE_WEB_UI = True
+ENABLE_DATA_ALTERING_API = True
 
 
 # Database config
 
 DB_NAME = "amagama"
-DB_USER = "postgres"
-DB_PASSWORD = ""
-#DB_HOST = "localhost"
-#DB_PORT = "5432"
+DB_USER = "usuario"
+DB_PASSWORD = "usuario"
+DB_HOST = "localhost"
+DB_PORT = "5432"
 
 
 # Database pool config
